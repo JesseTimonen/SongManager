dffb31f90538c8c8e556d6e35a44e49e765a3a51
JesseTimonen <jesse.timonen@outlook.com>
2021-09-19 18:45:06 +0300

    Modified unload Text

--- Scripts/Activation.lua
+++ Scripts/Activation.lua
@@ -1,12 +1,12 @@
 -- Activate Plugin --
 Plugins[pluginName].Load = function()
-	notification("Activated version " .. Plugins[pluginName]:GetVersion() .. " by Henna");
+	notification("Activated version " .. Plugins[pluginName]:GetVersion() .. " by Henna.");
 end
 
 -- Unload Plugin --
 Plugins[pluginName].Unload = function()
 	saveSettings();
-	notification("Unactivated");
+	notification("Plugin has been deactivated.");
 end
 
 -- Load settings and song library --
